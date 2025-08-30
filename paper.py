b3d626b63ca402bc5a853103e23645f64b92b26b
m3957 <86982093+m3957@users.noreply.github.com>
2025-08-30 14:06:33 -0400

    Cleanup de paper.py

--- paper.py
+++ paper.py
@@ -1,57 +1,52 @@
-import sys
-
-# TODO: Voice synthesis?
+from rich import print
 
 # Dictionnaire de l'horaire
-def endofprogram():
-    print("\n\n-- Fin du programme --")
 
 schedule = {
-    1: ["Période", "Période", "Période", "Période"],
-    2: ["Période", "Période", "Période", "Période"],
-    3: ["Période", "Période", "Période", "Période"],
-    4: ["Période", "Période", "Période", "Période"],
-    5: ["Période", "Période", "Période", "Période"],
-    6: ["Période", "Période", "Période", "Période"],
-    7: ["Période", "Période", "Période", "Période"],
-    8: ["Période", "Période", "Période", "Période"],
-    9: ["Période", "Période", "Période", "Période"]
+	1: ["Période", "Période", "Période", "Période"],
+	2: ["Période", "Période", "Période", "Période"],
+	3: ["Période", "Période", "Période", "Période"],
+	4: ["Période", "Période", "Période", "Période"],
+	5: ["Période", "Période", "Période", "Période"],
+	6: ["Période", "Période", "Période", "Période"],
+	7: ["Période", "Période", "Période", "Période"],
+	8: ["Période", "Période", "Période", "Période"],
+	9: ["Période", "Période", "Période", "Période"]
 }
 
 # Variables de compteur
-daycount = None
+daycount = 0
 periodcount = 0
 
 # Change la valeur de daycount pour une valeur choisie
 while daycount not in range(1,9):
-    try:
-        daycount = int(input("Jour d'école pour commencer (1-9) -----\n> "))
-    except ValueError:
-        print("\nRéponse invalide.\n\n")
-    except KeyboardInterrupt:
-        endofprogram()
-        sys.exit() # Quitte le programme
+	try:
+		print("[bold]Jour d'école pour commencer (1 à 9)[/bold]")
+		daycount = int(input("> "))
+	except ValueError:
+		print("\n[bold red]Réponse invalide.[/bold red]\n")
+	except KeyboardInterrupt:
+		exit(0)
 
-print("\nAppuie sur 󰌑 Enter pour continuer")
+print("\nAppuyez sur [bold]󰌑 Enter[/ bold] pour continuer.")
 
-# Change les variables et imprime la bonne
-# entrée depuis le dictionnaire
+# Change les variables et imprime la bonne entrée depuis le dictionnaire
 try:
-    while True:
-        input()
-        if periodcount >= 4:
-            periodcount = 1
-            if daycount >= 9:
-                daycount = 1
-            else:
-                daycount += 1
-        else:
-            periodcount += 1
-        print(f"""
-Jour {daycount} -----------------------
+	while True:
+		input()
+		if periodcount >= 4:
+			periodcount = 1
+			if daycount >= 9:
+				daycount = 1
+			else:
+				daycount += 1
+		else:
+			periodcount += 1
+		print(f"""
+Jour {daycount} ─────────────────
 Période {periodcount}: {schedule[daycount][periodcount - 1]}
-------------------------------
+─────────────────────────
 """)
 # Code pour le Ctrl/Cmd+C
 except KeyboardInterrupt:
-    endofprogram()
+	exit(0)
